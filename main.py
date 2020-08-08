ceadb82f7cb1554154ab2e1ae4bba8e082b66d8f
yaroslavpankovych <flower.moor@gmail.com>
2020-08-08 10:36:00 +0300

    Done

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 import os
+import random
+from datetime import datetime, timezone
 
 from git import Repo
 
+MAX_COMMITS_COUNT = 10
 repo = Repo(os.path.dirname(__file__))
 index = repo.index
 
-index.commit("test")
-repo.git.add("main.py")
-repo.remote().push("master:master")
+
+def update_readme():
+    last_commit_time = datetime.now(tz=timezone.utc)
+    with open("README.md", "w") as fp:
+        fp.write(f"# Last commit: {last_commit_time}")
+    index.add("README.md")
+
+
+def main():
+    for commit in range(1, random.randint(1, MAX_COMMITS_COUNT) + 1):
+        index.commit(f"commit #{commit}")
+    update_readme()
+    repo.remote().push("master:master")
+
+
+if __name__ == '__main__':
+    main()
