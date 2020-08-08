b766ddb735888febd44940c120bbc3e2b7ab5ed0
yaroslavpankovych <flower.moor@gmail.com>
2020-08-08 13:44:37 +0300

    fix

--- main.py
+++ main.py
@@ -13,13 +13,13 @@ def update_readme():
     last_commit_time = datetime.now(tz=timezone.utc)
     with open("README.md", "w") as fp:
         fp.write(f"# Last commit: {last_commit_time}")
-    repo.index.add([os.path.join(DIR_PATH, "README.md")])
+    repo.git.add([os.path.join(DIR_PATH, "README.md")], update=True)
 
 
 def main():
-    for commit in range(1, random.randint(1, MAX_COMMITS_COUNT) + 1):
-        repo.index.commit(f"automatic commit #{commit}")
     update_readme()
+    for commit in range(1, random.randint(1, MAX_COMMITS_COUNT) + 1):
+        repo.git.commit("-m", f"automatic commit #{commit}", "--allow-empty")
     repo.remote().push("master:master")
 
 
