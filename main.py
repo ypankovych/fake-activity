9e7090ee3d8a2bd0a1158fd9f646aeedec43e13c
yaroslavpankovych <flower.moor@gmail.com>
2020-08-08 13:35:31 +0300

    fix

--- main.py
+++ main.py
@@ -6,19 +6,18 @@ from git import Repo
 
 MAX_COMMITS_COUNT = 10
 repo = Repo(os.path.dirname(__file__))
-index = repo.index
 
 
 def update_readme():
     last_commit_time = datetime.now(tz=timezone.utc)
     with open("README.md", "w") as fp:
         fp.write(f"# Last commit: {last_commit_time}")
-    repo.git.add("README.md")
+    repo.index.add(["README.md"])
 
 
 def main():
     for commit in range(1, random.randint(1, MAX_COMMITS_COUNT) + 1):
-        index.commit(f"commit #{commit}")
+        repo.index.commit(f"automatic commit #{commit}")
     update_readme()
     repo.remote().push("master:master")
 
